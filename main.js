38f6ff7814188316c256a0a285453f28283bc910
syaifqr <bimasyaiful2@gmail.com>
2024-03-02 18:16:51 +0700

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "Cuma mau bilang",
+      "Selamat Malam Sayangku",
+      "Aku tau kamu capek, tapi tetep semangat yaa cantiku",
+      "Aku Sayang Banget Sama Kamu",
+      "I Love You Lailatul Fitriyah",
+    ],
+    typeSpeed: 90,
+  });
+}
